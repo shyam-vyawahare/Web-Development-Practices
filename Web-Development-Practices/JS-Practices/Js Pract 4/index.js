141d1ab75fd6245a1997c7450da147fb754dd797
Shyam Vyawahare <Shyamvyawahare1@gmail.com>
2025-12-12 16:42:01 +0530

    HTML, CSS, JS already practiced files
    HTML, CSS, JS already practiced files uploaded at the start of repo.

---
+++ Web-Development-Practices/JS-Practices/Js Pract 4/index.js
@@ -0,0 +1,49 @@
+console.log("I am a tutorial on Loops")
+
+let a = 1;
+
+// console.log(a)
+// console.log(a + 1)
+// console.log(a + 2)
+// console.log(a + 3) // The repeatative lines not recomanded
+
+// There are 5 loops in JavaScript
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i)
+// }
+
+// let obj = {
+//     name: "Ultrec",
+//     role: "Programmer",
+//     company: "CodeWithUltrex AI"
+// }
+
+// // 1. For in loop
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key, element) // Prints Key and value
+// }
+
+// for (const key in obj) {
+//     console.log(key) // Prints only Keys
+// }
+
+// // 2. For of loop
+// for (const c of "Ultrex") {
+//     console.log(c)
+// }
+
+// 3. While loop
+// let i = 0;
+// while (i < 6) {
+//     console.log(i)
+//     i++ // The increment condition is important to avoid infinite loop trap
+// }
+
+// 4. Do while loop (Mostly avoided and not used much)
+let i = 0;
+do {
+    console.log(i)
+    i++;
+} while (i < 6);
