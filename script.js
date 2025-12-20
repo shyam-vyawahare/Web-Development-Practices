783b11c083d1df926e64e35d3167f7d404de32f5
Shyam Vyawahare <Shyamvyawahare1@gmail.com>
2025-12-20 17:10:06 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display = 'flex'; // Assuming flex layout for the sidebar
-}
-
-function hideSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display = 'none'; // Hides the sidebar
-}
